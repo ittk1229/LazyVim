76b055e072370b2cd667ca10a16c7de12a677733
ittk1229 <s2010485@u.tsukuba.ac.jp>
2024-11-17 18:11:52 +0900

    背景の透明化と文字色の調整

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,6 +12,21 @@ return {
   },
   {
     "marko-cerovac/material.nvim",
+    opts = function(_, opts)
+      -- Transparent background
+      opts.disable = {
+        background = true,
+      }
+      -- Lighter fg colors
+      local fg_color = "#C0CED5"
+      opts.custom_colors = function(colors)
+        colors.editor.fg = "#ffffff"
+        colors.editor.fg_dark = fg_color
+        colors.syntax.variable = fg_color
+        colors.syntax.field = fg_color
+      end
+      return opts
+    end,
   },
   -- Set default colorscheme
   {
